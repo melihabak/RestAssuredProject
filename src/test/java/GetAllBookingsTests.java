cf2e2c27c4a8aab5611238a78a83f59d704f2c5d
melihabak <melihfurkanabak@gmail.com>
2024-05-31 11:28:33 +0300

    GetAllBookingsTests sinifı tamamnalndi pom.xml dosyasi duzenlendi

--- src/test/java/GetAllBookingsTests.java
+++ src/test/java/GetAllBookingsTests.java
@@ -1,2 +1,17 @@
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
 public class GetAllBookingsTests {
+    @Test
+    public void getAllBookingTest(){
+
+        given()
+                .when()
+                .get("https://restful-booker.herokuapp.com/booking")
+                .then()
+                .log().all() // Yaptigimiz cagrinin sonucunu gorebilecegiz
+                .statusCode(200);
+
+    }
 }
